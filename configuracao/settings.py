4ad1b0071e8249eee1170bd1d927e72d9ed07864
carolineberghahn <carolinesilva.ss036@academico.ifsul.edu.br>
2026-08-03 09:48:38 -0300

    Update style LUME Skin

--- configuracao/settings.py
+++ configuracao/settings.py
@@ -88,15 +88,17 @@ WSGI_APPLICATION = 'configuracao.wsgi.application'
 import os
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'lumeskin',                 # Mude para 'lume' se preferir usar o outro banco
+        'USER': 'root',
+        'PASSWORD': 'carol2007',       # Coloque a senha que definiu no MySQL Installer
+        'HOST': '127.0.0.1',                 # Mantenha exatamente entre aspas
+        'PORT': '3306',                      # Mantenha exatamente entre aspas
     }
 }
+
+
 
 
 # Password validation
